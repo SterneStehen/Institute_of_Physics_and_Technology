60dc5d6297ed9992a9efe17c20f4fd00c64dcba9
unknown <7353718@gmail.com>
2023-12-28 20:00:25 +0100

    insert + init

--- double_linke_list.c
+++ double_linke_list.c
@@ -34,6 +34,7 @@ void ft_print(Node * list)
 	}
 	printf("\n");
 }
+
 void ft_print_revers(Node * list)
 {
 	Node * p;
@@ -46,30 +47,62 @@ void ft_print_revers(Node * list)
 	printf("\n");
 }
 
+void ft_insert(Node *a, Node * t)
+{
+	Node *b = a->next;
+	t->next = b;
+	t->prev = a;
+	a->next = t;
+	b->prev = t;
+	
+	
+
+	// tmp->next = t;
+	// t->prev = tmp;
+	// tmp = tmp->next;
+	// tmp = tmp->next;
+	// t->next = tmp;
+	// tmp->prev = t;
+	// free(tmp);
+}
+
+int ft_is_empty(Node * list)
+{
+	return (list->next == list->prev);
+}
+
+void ft_init(Node * list)
+{
+	list->next = list;
+	list->prev = list;
+}
+
 int main()
 {
-	Node z, a = {3}, b = {17}, c = {21};
+	Node z = {0}, a = {3}, b = {17}, c = {21}, u = {10};
 	Node * list = &z;
 	
-	a.next = &b;
-	a.prev = &z;
-	b.next = &c;
-	b.prev = &a;
-	c.next = &z;
-	c.prev = &b;
-	z.next = &a;
-	z.prev = &c;
+	// a.next = &b;
+	// a.prev = &z;
+	// b.next = &c;
+	// b.prev = &a;
+	// c.next = &z;
+	// c.prev = &b;
+	// z.next = &a;
+	// z.prev = &c;
+
+	ft_init(list);
+
+	ft_insert(&z, &a);
+	ft_print(list);
+	ft_insert(&a, &b);
+	ft_print(list);
+	ft_insert(&b, &c);
+	ft_print(list);
+	ft_insert(&a, &u);
 
 	ft_print(list);
 	ft_print_revers(list);
 	ft_print_dbg(list);
-	
-	// printf("%d\n", p->num);
-	// p = p->next;
-	// printf("%d\n", p->num);
-	// p = p->next;
-	// printf("%d\n", p->num);
-
-	
 	
 }
